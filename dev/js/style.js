b079030f4b2940aac58a6ae582a9a51acd61e21e
Diary2017 <divawu99763@outlook.com>
2021-05-13 17:06:48 +0800

    [feat]新增功能，點選漢堡  -> 彈窗消失 -> 漢堡選單出現 -> 移除blur

--- dev/js/style.js
+++ dev/js/style.js
@@ -2,3 +2,14 @@
 $('.btn_close').click(function(){
     $('.wrapper').removeClass('blur');  
 })
+
+//點選漢堡，彈窗消失
+$('.ham').click(function(){
+    $('.popUps').fadeOut(100);
+    // $('.wrapper').removeClass('blur');  
+});
+//在漢堡選單出現時，移除blur
+
+$('#ham_icon').click(function(){
+    $('.wrapper').removeClass('blur');
+});
