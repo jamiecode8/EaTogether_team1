9c1a381c6d9178509d0f6feb45cc2068191e1465
jamiecode8 <jamiechiu2020@gmail.com>
2021-05-14 22:10:42 +0800

    [feat] 修改商家登入註冊jQuery, 修改footer 連結，先連到登入註冊頁面

--- dev/js/login_store.js
+++ dev/js/login_store.js
@@ -1,16 +1,27 @@
  // 登入註冊切換
- $('#btn_signup').click(function(){
-    $(this).addClass('areaShow');
-    $('#btn_login').removeClass('areaShow');
-    $('#login_block').addClass('areaHide');
-    $('#signUp_block').removeClass('areaHide');
+// 商家登入彈窗-----------------------------
+// 按送出 : 先不寫功能
+
+
+
+ // 切換註冊時，商家登入隱藏、商家註冊出現
+ $('#btn_signupStore').click(function(){
+    $('#store_signUp').removeClass('blockNone').addClass('blockBlock');
+    $('#storeLogin_popups').removeClass('blockBlock').addClass('blockNone');
 });
 
-$('#btn_login').click(function(){
-    $(this).addClass('areaShow');
-    $('#btn_signup').removeClass('areaShow');
-    $('#signUp_block').addClass('areaHide');
-    $('#login_block').removeClass('areaHide');
+// 按忘記密碼，出現忘記密碼的彈窗，覆蓋住登入彈窗
+
+// 商家註冊(商家資訊)彈窗----------------------
+// 商家註冊頁面，按右上角XX, 回商家登入彈窗
+$('#store_signUp_close').click(function(){
+    $('#store_signUp').removeClass('blockBlock').addClass('blockNone');
+    $('#storeLogin_popups').removeClass('blockNone').addClass('blockBlock');
+});
+// 商家註冊頁面，按取消，都回商家登入彈窗
+$('#store_signUp_back').click(function(){
+    $('#store_signUp').removeClass('blockBlock').addClass('blockNone');
+    $('#storeLogin_popups').removeClass('blockNone').addClass('blockBlock');
 });
 
 // 下拉選單-客單價
@@ -26,11 +37,8 @@ $('#login_station_options li').click(function(){
 
 })
 
-
-
 // 下拉選單-鄰近站點
 $('.login_store_station_arrow').click(function (e) {
-    // e.preventDefault();
 $('.login_store_station_select').toggleClass('on');
 });
 
