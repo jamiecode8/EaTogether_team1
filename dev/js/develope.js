df039343f751b1a81b97333de3f1f21d1a3464c6
Diary2017 <divawu99763@outlook.com>
2021-05-16 00:40:41 +0800

    [feat]刪除多餘class

--- dev/js/develope.js
+++ dev/js/develope.js
@@ -3,12 +3,12 @@
                 <div class="develope_form jamie">
                     <h2>職稱 | 組長</h2>
                     <br>
-                    <h1 class="h1_develope">邱博英</h1>
+                    <h1>邱博英</h1>
                     <br>
                     <ul>
-                        <li class="li_develope">廠商專區頁面</li>
-                        <li class="li_develope">登入系統</li>
-                        <li class="li_develope">開發架構建置</li>
+                        <li>廠商專區頁面</li>
+                        <li>登入系統</li>
+                        <li>開發架構建置</li>
                     </ul>
                     <br>
                     <a href="https://jamiecode8.github.io/tfd101010/" target="_blank">
@@ -23,12 +23,12 @@
                 <div class="develope_form chris">
                     <h2>職稱 | 組員</h2>
                     <br>
-                    <h1 class="h1_develope">楊書旻</h1>
+                    <h1>楊書旻</h1>
                     <br>
                     <ul>
-                        <li class="li_develope">廠商專區頁面</li>
-                        <li class="li_develope">登入系統</li>
-                        <li class="li_develope">開發架構建置</li>
+                        <li>廠商專區頁面</li>
+                        <li>登入系統</li>
+                        <li>開發架構建置</li>
                     </ul>
                     <br>
                     <a href="https://diary2017.github.io/Gladyme/" target="_blank">
@@ -43,12 +43,12 @@
                 <div class="develope_form mimi">
                     <h2>職稱 | 組員</h2>
                     <br>
-                    <h1 class="h1_develope">賴彥勳</h1>
+                    <h1>賴彥勳</h1>
                     <br>
                     <ul>
-                        <li class="li_develope">廠商專區頁面</li>
-                        <li class="li_develope">登入系統</li>
-                        <li class="li_develope">開發架構建置</li>
+                        <li>廠商專區頁面</li>
+                        <li>登入系統</li>
+                        <li>開發架構建置</li>
                     </ul>
                     <br>
                     <a href="https://judy8108.github.io/WD101003/landingpage.html" target="_blank">
@@ -63,12 +63,12 @@
                 <div class="develope_form levis">
                     <h2>職稱 | 組員</h2>
                     <br>
-                    <h1 class="h1_develope">顏宏鈞</h1>
+                    <h1>顏宏鈞</h1>
                     <br>
                     <ul>
-                        <li class="li_develope">廠商專區頁面</li>
-                        <li class="li_develope">登入系統</li>
-                        <li class="li_develope">開發架構建置</li>
+                        <li>廠商專區頁面</li>
+                        <li>登入系統</li>
+                        <li>開發架構建置</li>
                     </ul>
                     <br>
                     <a href="https://levisedwin.github.io/tdf10121/index.html" target="_blank">
@@ -83,12 +83,12 @@
                 <div class="develope_form bobo">
                     <h2>職稱 | 組員</h2>
                     <br>
-                    <h1 class="h1_develope">蕭洛棠</h1>
+                    <h1>蕭洛棠</h1>
                     <br>
                     <ul>
-                        <li class="li_develope">廠商專區頁面</li>
-                        <li class="li_develope">登入系統</li>
-                        <li class="li_develope">開發架構建置</li>
+                        <li>廠商專區頁面</li>
+                        <li>登入系統</li>
+                        <li>開發架構建置</li>
                     </ul>
                     <br>
                     <a href="https://bobo52400.github.io/Lakeside-Chalet/" target="_blank">
@@ -103,12 +103,12 @@
                 <div class="develope_form pamela">
                     <h2>職稱 | 組員</h2>
                     <br>
-                    <h1 class="h1_develope">李靜瑩</h1>
+                    <h1>李靜瑩</h1>
                     <br>
                     <ul>
-                        <li class="li_develope">廠商專區頁面</li>
-                        <li class="li_develope">登入系統</li>
-                        <li class="li_develope">開發架構建置</li>
+                        <li>廠商專區頁面</li>
+                        <li>登入系統</li>
+                        <li>開發架構建置</li>
                     </ul>
                     <br>
                     <a href="https://ginny-li.github.io/tanaka/loading.html" target="_blank">
