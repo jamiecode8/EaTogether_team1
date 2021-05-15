c9d9e66189286011b25b7d3a2f0e01a208c37a38
judy8108 <judy8889888@gmail.com>
2021-05-15 14:12:48 +0800

    [feat]新增衣櫃RWD跟輪播功能

--- dev/js/myUFO_wardrobeu.js
+++ dev/js/myUFO_wardrobeu.js
@@ -27,3 +27,19 @@ $(function(){
 //     prevEl: '.swiper-button-prev',
 //   },
 // });
+$('.carousel[data-type="multi"] .item').each(function() {
+	var next = $(this).next();
+	if (!next.length) {
+		next = $(this).siblings(':first');
+	}
+	next.children(':first-child').clone().appendTo($(this));
+
+	for (var i = 0; i < 2; i++) {
+		next = next.next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+
+		next.children(':first-child').clone().appendTo($(this));
+	}
+});
