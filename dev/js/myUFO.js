f815243de43987ff4c387daed144fa2ad63debd5
Diary2017 <divawu99763@outlook.com>
2021-05-26 18:31:18 +0800

    [feat]新增我的外食星人，建立角色功能。

--- dev/js/myUFO.js
+++ dev/js/myUFO.js
@@ -8,30 +8,30 @@
                 $(this.parentNode.parentNode).fadeOut(200);
             });
             //點選其他部位
-            //眼睛
-            $('.choiceList_eye').click(function (){
-                $('.choiceList_eye').addClass('on');
-                $('.choiceList_body').addClass('off');
-                $('.choiceList_mouth').removeClass('on');
-                $('.choiceOrgan').fadeOut(100);
-                $('.myEye').fadeIn(50);
-            });
-            //嘴巴
-            $('.choiceList_mouth').click(function (){
-                $('.choiceList_mouth').addClass('on');
-                $('.choiceList_eye').removeClass('on');
-                $('.choiceList_body').addClass('off');
-                $('.choiceOrgan').fadeOut(100);
-                $('.myMouth').fadeIn(50);
-            });
-            //身體
-            $('.choiceList_body').click(function (){
-                $('.choiceList_mouth').removeClass('on');
-                $('.choiceList_eye').removeClass('on');
-                $('.choiceList_body').removeClass('off');
-                $('.choiceOrgan').fadeOut(100);
-                $('.myBody').fadeIn(50);
-            });
+            // //眼睛
+            // $('.choiceList_eye').click(function (){
+            //     $('.choiceList_eye').addClass('on');
+            //     $('.choiceList_body').addClass('off');
+            //     $('.choiceList_mouth').removeClass('on');
+            //     $('.choiceOrgan').fadeOut(100);
+            //     $('.myEye').fadeIn(50);
+            // });
+            // //嘴巴
+            // $('.choiceList_mouth').click(function (){
+            //     $('.choiceList_mouth').addClass('on');
+            //     $('.choiceList_eye').removeClass('on');
+            //     $('.choiceList_body').addClass('off');
+            //     $('.choiceOrgan').fadeOut(100);
+            //     $('.myMouth').fadeIn(50);
+            // });
+            // //身體
+            // $('.choiceList_body').click(function (){
+            //     $('.choiceList_mouth').removeClass('on');
+            //     $('.choiceList_eye').removeClass('on');
+            //     $('.choiceList_body').removeClass('off');
+            //     $('.choiceOrgan').fadeOut(100);
+            //     $('.myBody').fadeIn(50);
+            // });
 
             // ---- 優惠卷 --------------------------
             $('#coupon_wardrobe').click(function(){
