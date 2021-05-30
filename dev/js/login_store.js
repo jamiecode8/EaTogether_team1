a658f131d1819f71ae80694216230be116950850
jamiecode8 <jamiechiu2020@gmail.com>
2021-05-30 19:55:08 +0800

    [feat] 新增照片上傳的功能，按鈕可用，拖曳無效

--- dev/js/login_store.js
+++ dev/js/login_store.js
@@ -60,19 +60,270 @@ $('.showPassword').click(function(){
 
 // 上傳照片
 function doFirst(){
-    //先跟畫面產生關連，再建事件聆聽功能
     document.getElementById('theFile1').onchange = fileChange;
+    document.getElementById('theFile1').ondragover = dragOver;
+    document.getElementById('theFile1').ondrop = dropped;
+    document.getElementById('theFile2').onchange = fileChange2;
+    // document.getElementById('theFile2').ondragover = dragOver2;
+    // document.getElementById('theFile2').ondrop = dropped2;
+    document.getElementById('theFile3').onchange = fileChange3;
+    // document.getElementById('theFile3').ondragover = dragOver3;
+    // document.getElementById('theFile3').ondrop = dropped3;
+    document.getElementById('theFile4').onchange = fileChange4;
+    // document.getElementById('theFile4').ondragover = dragOver4;
+    // document.getElementById('theFile4').ondrop = dropped4;
+    document.getElementById('theFile5').onchange = fileChange5;
+    // document.getElementById('theFile5').ondragover = dragOver5;
+    // document.getElementById('theFile5').ondrop = dropped5;
+    document.getElementById('theFile6').onchange = fileChange6;
+    // document.getElementById('theFile6').ondragover = dragOver6;
+    // document.getElementById('theFile6').ondrop = dropped6;
   }
+  //按鈕上傳照片功能   
   function fileChange(){
     let file = document.getElementById('theFile1').files[0];
+    // let file = document.querySelectorAll('.btn_upload').files[0];
     let readFile = new FileReader();
     readFile.readAsDataURL(file);
     readFile.addEventListener('load',function(){
       let image = document.getElementById('store_image1');
       image.src = readFile.result;
-      image.style.maxWidth = '300px';
-      image.style.maxHeight = '300px';
+      if( image.src != ""){          
+          let btn_Upload = document.getElementsByClassName('picUpload')[0];
+          image.style.maxWidth = '300px';
+          image.style.maxHeight = '300px';
+          btn_Upload.style.left = '240px';
+          btn_Upload.style.top = '210px';
+      }
     });
-  
   }
+ //拖曳上傳照片功能  
+  function dragOver(e){
+    e.preventDefault();
+  }
+  
+  function dropped(e){
+    e.preventDefault();
+    let file = e.dataTransfer.files[0];
+  
+    let readFile = new FileReader();
+    readFile.readAsDataURL(file);
+    readFile.addEventListener('load',function(){
+      let image = document.getElementById('store_image1');
+      image.src = readFile.result;
+      if( image.src != ""){          
+        let btn_Upload = document.getElementsByClassName('picUpload')[0];
+        image.style.maxWidth = '300px';
+        image.style.maxHeight = '300px';
+        btn_Upload.style.left = '240px';
+        btn_Upload.style.top = '210px';
+    }
+    });
+  }
+
+//   以下有時間來研究怎麼改成迴圈------------------------------------------
+function fileChange2(){
+    let file = document.getElementById('theFile2').files[0];
+    // let file = document.querySelectorAll('.btn_upload').files[0];
+    let readFile = new FileReader();
+    readFile.readAsDataURL(file);
+    readFile.addEventListener('load',function(){
+      let image = document.getElementById('store_image2');
+      image.src = readFile.result;
+      if( image.src != ""){          
+          let btn_Upload2 = document.getElementById('picUpload2')[0];
+          image.style.maxWidth = '300px';
+          image.style.maxHeight = '300px';
+          btn_Upload2.style.left = '240px';
+          btn_Upload2.style.top = '210px';
+      }
+    });
+  }
+//  //拖曳上傳照片功能  
+//   function dragOver2(e){
+//     e.preventDefault();
+//   }
+  
+//   function dropped2(e){
+//     e.preventDefault();
+//     let file = e.dataTransfer.files[0];
+  
+//     let readFile = new FileReader();
+//     readFile.readAsDataURL(file);
+//     readFile.addEventListener('load',function(){
+//       let image = document.getElementById('store_image2');
+//       image.src = readFile.result;
+//       if( image.src != ""){          
+//         let btn_Upload = document.getElementsByClassName('picUpload')[0];
+//         image.style.maxWidth = '300px';
+//         image.style.maxHeight = '300px';
+//         btn_Upload.style.left = '240px';
+//         btn_Upload.style.top = '210px';
+//     }
+//     });
+//   }
+
+  function fileChange3(){
+    let file = document.getElementById('theFile3').files[0];
+    // let file = document.querySelectorAll('.btn_upload').files[0];
+    let readFile = new FileReader();
+    readFile.readAsDataURL(file);
+    readFile.addEventListener('load',function(){
+      let image = document.getElementById('store_image3');
+      image.src = readFile.result;
+      if( image.src != ""){          
+        let btn_Upload = document.getElementById('picUpload3')[0];
+          image.style.maxWidth = '300px';
+          image.style.maxHeight = '300px';
+          btn_Upload.style.left = '240px';
+          btn_Upload.style.top = '210px';
+      }
+    });
+  }
+//  //拖曳上傳照片功能  
+//   function dragOver3(e){
+//     e.preventDefault();
+//   }
+  
+//   function dropped3(e){
+//     e.preventDefault();
+//     let file = e.dataTransfer.files[0];
+  
+//     let readFile = new FileReader();
+//     readFile.readAsDataURL(file);
+//     readFile.addEventListener('load',function(){
+//       let image = document.getElementById('store_image3');
+//       image.src = readFile.result;
+//       if( image.src != ""){          
+//         let btn_Upload = document.getElementsByClassName('picUpload')[0];
+//         image.style.maxWidth = '300px';
+//         image.style.maxHeight = '300px';
+//         btn_Upload.style.left = '240px';
+//         btn_Upload.style.top = '210px';
+//     }
+//     });
+//   }
+
+  function fileChange4(){
+    let file = document.getElementById('theFile4').files[0];
+    // let file = document.querySelectorAll('.btn_upload').files[0];
+    let readFile = new FileReader();
+    readFile.readAsDataURL(file);
+    readFile.addEventListener('load',function(){
+      let image = document.getElementById('store_image4');
+      image.src = readFile.result;
+      if( image.src != ""){          
+        let btn_Upload = document.getElementById('picUpload4')[0];
+          image.style.maxWidth = '300px';
+          image.style.maxHeight = '300px';
+          btn_Upload.style.left = '240px';
+          btn_Upload.style.top = '210px';
+      }
+    });
+  }
+//  //拖曳上傳照片功能  
+//   function dragOver4(e){
+//     e.preventDefault();
+//   }
+  
+//   function dropped4(e){
+//     e.preventDefault();
+//     let file = e.dataTransfer.files[0];
+  
+//     let readFile = new FileReader();
+//     readFile.readAsDataURL(file);
+//     readFile.addEventListener('load',function(){
+//       let image = document.getElementById('store_image4');
+//       image.src = readFile.result;
+//       if( image.src != ""){          
+//         let btn_Upload = document.getElementsByClassName('picUpload')[0];
+//         image.style.maxWidth = '300px';
+//         image.style.maxHeight = '300px';
+//         btn_Upload.style.left = '240px';
+//         btn_Upload.style.top = '210px';
+//     }
+//     });
+//   }
+
+  function fileChange5(){
+    let file = document.getElementById('theFile5').files[0];
+    // let file = document.querySelectorAll('.btn_upload').files[0];
+    let readFile = new FileReader();
+    readFile.readAsDataURL(file);
+    readFile.addEventListener('load',function(){
+      let image = document.getElementById('store_image5');
+      image.src = readFile.result;
+      if( image.src != ""){          
+        let btn_Upload = document.getElementById('picUpload5')[0];
+          image.style.maxWidth = '300px';
+          image.style.maxHeight = '300px';
+          btn_Upload.style.left = '240px';
+          btn_Upload.style.top = '210px';
+      }
+    });
+  }
+//  //拖曳上傳照片功能  
+//   function dragOver5(e){
+//     e.preventDefault();
+//   }
+  
+//   function dropped5(e){
+//     e.preventDefault();
+//     let file = e.dataTransfer.files[0];
+  
+//     let readFile = new FileReader();
+//     readFile.readAsDataURL(file);
+//     readFile.addEventListener('load',function(){
+//       let image = document.getElementById('store_image5');
+//       image.src = readFile.result;
+//       if( image.src != ""){          
+//         let btn_Upload = document.getElementsByClassName('picUpload')[0];
+//         image.style.maxWidth = '300px';
+//         image.style.maxHeight = '300px';
+//         btn_Upload.style.left = '240px';
+//         btn_Upload.style.top = '210px';
+//     }
+//     });
+//   }
+
+  function fileChange6(){
+    let file = document.getElementById('theFile6').files[0];
+    // let file = document.querySelectorAll('.btn_upload').files[0];
+    let readFile = new FileReader();
+    readFile.readAsDataURL(file);
+    readFile.addEventListener('load',function(){
+      let image = document.getElementById('store_image6');
+      image.src = readFile.result;
+      if( image.src != ""){          
+        let btn_Upload = document.getElementById('picUpload6')[0];
+          image.style.maxWidth = '300px';
+          image.style.maxHeight = '300px';
+          btn_Upload.style.left = '240px';
+          btn_Upload.style.top = '210px';
+      }
+    });
+  }
+//  //拖曳上傳照片功能  
+//   function dragOver6(e){
+//     e.preventDefault();
+//   }
+  
+//   function dropped6(e){
+//     e.preventDefault();
+//     let file = e.dataTransfer.files[0];
+  
+//     let readFile = new FileReader();
+//     readFile.readAsDataURL(file);
+//     readFile.addEventListener('load',function(){
+//       let image = document.getElementById('store_image6');
+//       image.src = readFile.result;
+//       if( image.src != ""){          
+//         let btn_Upload = document.getElementsByClassName('picUpload')[0];
+//         image.style.maxWidth = '300px';
+//         image.style.maxHeight = '300px';
+//         btn_Upload.style.left = '240px';
+//         btn_Upload.style.top = '210px';
+//     }
+//     });
+//   }
   window.addEventListener('load',doFirst);
