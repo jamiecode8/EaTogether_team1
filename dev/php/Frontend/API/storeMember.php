7ed98ba5ae822e22d55f5d6c17d9982e25301e09
jamiecode8 <jamiechiu2020@gmail.com>
2021-06-05 00:57:43 +0800

    [feat] 商家頁面 成功渲染完整文字商家資訊

--- dev/php/Frontend/API/storeMember.php
+++ dev/php/Frontend/API/storeMember.php
@@ -1,23 +1,23 @@
 <?php
-    include("../../Lib/Util.php");
-    include("../../Lib/Member.php");
+   include("../../Lib/Util.php");
+   include("../../Lib/Member.php");
 
-    //建立SQL
-    $sql = "SELECT 
-    r.store_name, r.store_address, r.store_tel,
-    r.price_id, t.station_name, r.store_submit
-    FROM store r natural join station t
-    where contact_tel = ?";
+   //建立SQL
+   $sql = "SELECT 
+   r.store_name, r.store_address, r.store_tel,
+   r.price_id, t.station_name, r.store_submit,  r.store_level,
+   r.store_tag1, r.store_tag2, r.store_tag3,
+   r.image_img, r.store_img, r.menu_img, r.food1_img, r.food2_img, r.food3_img
+   FROM store r natural join station t
+   where contact_tel = ?";
 
-    //執行
-    $statement = getPDO()->prepare($sql);
-    $statement->bindValue(1 , getStoreMemberID()); 
-    // $statement->bindValue(2 , getStoreMemberID()); 
-    $statement->execute();
-    $data = $statement->fetchAll();
+   //執行
+   $statement = getPDO()->prepare($sql);
+   $statement->bindValue(1 , getStoreMemberID()); 
+   $statement->execute();
+   $data = $statement->fetchAll();
 
- 
-    //回傳json
-    // echo 123;
-    echo json_encode($data);
+
+   //回傳json
+   echo json_encode($data);
 ?>
