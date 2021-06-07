be1e77738ec4db068401dc446b93977ed7fe193a
bobo52400 <bobo524000@gmail.com>
2021-06-07 14:19:53 +0800

    [docs]新增myphoto_post_pic.php

--- dev/js/myphoto_post_pic.js
+++ dev/js/myphoto_post_pic.js
@@ -15,8 +15,20 @@ function fileChange(){
         pho_image.style.top = '50%';
         pho_image.style.left = '50%';
         pho_image.style.transform = 'translate(-50%,-50%)';
-
+        
+        if( pho_image.src != ""){
+            let pho_input_change = document.getElementsByClassName('pho_input_change')[0]; 
+            let pho_picUpload = document.getElementsByClassName('pho_picUpload')[0]; 
+            let pho_picForm = document.getElementsByClassName('pho_picForm')[0];  
+            pho_picUpload.style.top = '86%';
+            pho_picUpload.style.left = '86%';
+            pho_input_change.style.top = '86%';
+            pho_input_change.style.left = '86%';
+            pho_picForm.style.display = 'none';
+        };
     });
-}
+};
 window.addEventListener('load',doFirst);
+
+
 
