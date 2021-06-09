725b391e232fd6fc1262829c5fc29c922fea2c78
Diary2017 <divawu99763@outlook.com>
2021-06-10 04:13:09 +0800

    [feat] 登入/登出系統

--- dev/php/Frontend/API/Member_Login_Check.php
+++ dev/php/Frontend/API/Member_Login_Check.php
@@ -1,6 +1,8 @@
-<?php    
+<?php
 	include("../../Lib/Member.php");
 
-    //登入檢查
-    echo getMemberID();
+	// 顯示會員資訊   str 為回傳值，有登入回傳暱稱，沒登入回傳空字串
+    $str = getMemberName();
+	// 回傳空值 執行 ：  前面，回傳不是空值 執行 ： 後面
+	echo $str == "" ? "" : $str;
 ?>
