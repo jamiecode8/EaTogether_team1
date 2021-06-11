ff7c508ddb181b550177ee8c782318c9bfa3fd10
jamiecode8 <jamiechiu2020@gmail.com>
2021-06-11 16:59:47 +0800

    [feat] 商家後台拆成3頁、編輯功能完成

--- dev/php/Backend/API/storeUpdate.php
+++ dev/php/Backend/API/storeUpdate.php
@@ -3,29 +3,28 @@
 
     //取得POST過來的值
     $SID = $_POST["storeId"]; //PK
-
-    $editStatus = $_POST["editStatus_id"];   //商家狀態
-    $editSubmit = $_POST["editSubmit_id"];   //審核進度
-    $editStatus = $_POST["editLevel_id"];   //商家等級
+    $editStatus = $_POST["editStatus"];   //商家狀態
+    $editSubmit = $_POST["editSubmit"];   //審核進度
+    $editLevel = $_POST["editLevel"];   //商家等級
     
+
     //返回訊息文字
     $message = "修改成功!";   
 
-    //建立SQL
+    // //建立SQL
     $sql = "UPDATE store set 
     store_status = ?, 
     store_submit = ?, 
-    store_level = ?, 
+    store_level = ?
     WHERE store_id = ?";
     
-    //執行
+    // //執行
     $statement = getPDO()->prepare($sql);
     $statement->bindValue(1 , $editStatus);     
     $statement->bindValue(2 , $editSubmit);
-    $statement->bindValue(3 , $editStatus);
+    $statement->bindValue(3 , $editLevel);
     $statement->bindValue(4 , $SID);
     $statement->execute();
     
-    //導頁    
-    // echo "<script>alert('".$message."'); location.href = '../Product.html';</script>";
-?>
+    echo $message;
+    ?>
