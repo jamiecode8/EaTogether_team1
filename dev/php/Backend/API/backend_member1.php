643ce4226500aa558346fbf8b2cee51b9c2c042f
LevisEdwin <charlie19908@gmail.com>
2021-06-14 01:48:45 +0800

    [feat] sql語法更新

--- dev/php/Backend/API/backend_member1.php
+++ dev/php/Backend/API/backend_member1.php
@@ -2,9 +2,15 @@
     include("../../Lib/Util.php");
 
     //建立SQL
-    $sql = "SELECT * FROM member WHERE  member_id = ?";
- 
-   
+    $sql = "SELECT * 
+    FROM member  
+    -- r  JOIN station t  ON r.station = t.station_id
+    JOIN station ON  member.station = station.station_id
+    JOIN tag ON member.tag1 = tag.tag_id
+    -- JOIN tag ON member.tag2 = tag.tag_id
+    -- JOIN tag ON member.tag3 = tag.tag_id
+    WHERE member_id =  ?";
+
 
     //執行
     $statement = getPDO()->prepare($sql);
