5d80cd99b652be6dfe2b21b9e2e5627eec99955c
LevisEdwin <charlie19908@gmail.com>
2021-06-14 01:49:09 +0800

    [feat]php語法更新

--- dev/php/Backend/API/backend_member.php
+++ dev/php/Backend/API/backend_member.php
@@ -1,18 +1,29 @@
 <?php
     
     include("../../Lib/Util.php");
+    
+    $keyword = $_POST['keyword'];
+    if($keyword == ''){
+        //建立SQL
+        $sql = "SELECT * FROM member";
+        //執行
+        $statement = getPDO()->prepare($sql);
+    }else{
+        $sql = "SELECT * FROM member WHERE member_name like ? " ;
+        $statement = getPDO()->prepare($sql);
+        $statement->bindValue(1, "%".$keyword."%");
+    }
+    
 
-    //建立SQL
-    $sql = "SELECT * FROM member";
+    
     // $sql = "SELECT * FROM member WHERE member_status = 1";
     
    
 
-    //執行
-    $statement = getPDO()->prepare($sql);
-    $statement->execute();
-    $data = $statement->fetchAll();
+        //執行
+        $statement->execute();
+        $data = $statement->fetchAll();
 
-    //回傳json
-    echo json_encode($data);
+        //回傳json
+        echo json_encode($data);
 ?>
